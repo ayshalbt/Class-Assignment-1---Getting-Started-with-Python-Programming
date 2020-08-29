6854d46481801ef18a91909b7d93ff9b010c87b1
ayshalbt <70258463+ayshalbt@users.noreply.github.com>
2020-08-29 09:47:48 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,38 @@
+2
+print("Name: AYSHA S")
+print("Address:JNRA 80 kallatumukku Manacaud PO, Trivandrum")
+print("telephone: 0471-2464472")
+
+3
+name = "Aysha S"
+print("Your name is",name)
+
+
+4
+width = int(input("Enter the width: "))
+height = int(input("Enter the height: "))
+area = width*height
+print("The area is ",area,"square units")
+
+5
+base = int(input("enter the base: "))
+height = int(input("enter the height: "))
+area = 0.5*base*height
+print("The area is",area,"square units")
+
+6
+radius = int(input("enter the radius: "))
+area = 3.14*radius**2
+print("The area of circle is",area,"square units")
+
+7
+name = input("enter the name: ")
+age = int(input("enter the age: "))
+print("Name is",name,"and age is",age)
+
+8
+num = int(input("enter a number: "))
+num = num+1
+
+9
+name = input("enter your first name")
